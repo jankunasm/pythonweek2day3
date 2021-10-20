ed4c8a04fe7bdfcae9ee51751a8530ab6e7e3357
jankunasm <jankunasmatas11@gmail.com>
2021-10-20 16:40:40 -0500

    Homework commit.

---
+++ hwmodule1.py
@@ -0,0 +1,6 @@
+def find_circum():
+    
+    PI = 3.14
+    radius = float(input("Enter radius of a circle : "))
+    circumfrence = 2 * PI * radius
+    return circumfrence
---
+++ hwmodule2.py
@@ -0,0 +1,6 @@
+def find_sqfoot():
+
+    width = float(input("Enter width of a house : "))
+    length = float(input("Enter length of a house : "))
+    area = width * length
+    return area
